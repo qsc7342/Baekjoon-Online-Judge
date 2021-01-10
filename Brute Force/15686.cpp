7747521baad48e8548a4490af882d3ba30c06c03
UmTaeho-Evaporation <nintendo7342@naver.com>
2021-01-11 01:48:57 +0900

    치킨 배달 / 완전 탐색

---
+++ Brute Force/15686.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define pb push_back
+#define mp make_pair
+#define MOD 1000000007
+#define cont continue
+#define rep(i, n) for(int i = 0 ; i < (n) ; i++)
+#define all(x) (x).begin(), (x).end()
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+const ll INF = 1e15;
+
+int T;
+
+int main() {
+  cin.tie(NULL);
+  ios_base::sync_with_stdio(false);
+
+  while(T--) {
+    int A, B;
+    queue<pair<int, string> > q;
+
+    q.push(mp(A, ""))
+  }
+}
