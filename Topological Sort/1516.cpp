1ef8a5f22796c1f6bf746ea15528fa42cc99617f
UmTaeho-Evaporation <nintendo7342@naver.com>
2021-01-11 00:56:17 +0900

    게임 개발 / 위상 정렬

---
+++ Topological Sort/1516.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define FASTIO() ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define TC() int TEST_CASE;cin>>TEST_CASE;for(int TEST_NUM=1;TEST_NUM<=TEST_CASE;TEST_NUM++)
+#define fst first
+#define snd second
+#define int long long
+#define endl '\n'
+#define pb push_back
+#define mp make_pair
+#define MOD 1000000007
+#define cont continue
+#define rep(i, n) for(int i = 0 ; i < (n) ; i++)
+#define sc(x) int x; cin >> x;
+#define sz(x) x.size()
+#define all(x) (x).begin(), (x).end()
+#define PQ(T) priority_queue<T> // 맥스힙
+#define GPQ(T) priority_queue<T,vector<T>,greater<T>> // 민힙
+using namespace std;
+typedef pair<int,int> pii;
+const int INF = 1e15;
+
+
+signed main() {
+  FASTIO();
+  sc(N);
+
+  cout << (int) (3 * (2 + N)) << endl;
+
+  for(int i = 0 ; i <= 2 ; i++) {
+    for(int j = 0 ; j <= 1 + N ; j++) {
+      cout << i << ' ' << j << endl;
+    }
+  }
+  return 0;
+}
