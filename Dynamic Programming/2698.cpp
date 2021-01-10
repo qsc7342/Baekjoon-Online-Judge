531f4281e23cda8a611ab3a6b527d324a25ec45c
UmTaeho-Evaporation <nintendo7342@naver.com>
2021-01-11 01:16:40 +0900

    인접한 비트의 개수 / DP

---
+++ Dynamic Programming/2698.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define pb push_back
+#define mp make_pair
+#define MOD 1000000007
+#define cont continue
+#define rep(i, n) for(int i = 0 ; i < (n) ; i++)
+#define all(x) (x).begin(), (x).end()
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+const ll INF = 987654321;
+
+int T;
+bool is
+int main() {
+  cin.tie(NULL);
+  ios_base::sync_with_stdio(false);
+  while(T--) {
+    int n;
+    cin >> n;
+    cout << n / 2 << endl;
+  }
+} 
