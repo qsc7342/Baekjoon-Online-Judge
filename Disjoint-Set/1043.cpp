93f401187ccf67fde52a9b7829206f9165e81041
UmTaeho-Evaporation <nintendo7342@naver.com>
2021-01-11 00:45:02 +0900

    분리 집합

---
+++ Disjoint-Set/1043.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define pb push_back
+#define mp make_pair
+#define MOD 1000000007
+#define cont continue
+#define rep(i, n) for(int i = 0 ; i < (n) ; i++)
+#define all(x) (x).begin(), (x).end()
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+const ll INF = 987654321;
+ 
+ll N, M;
+bool cur[51] = {};
+bool party2[51] = {};
+int main() {
+  cin.tie(NULL);
+  ios_base::sync_with_stdio(false); 
+
+  cout << (-2 * 0.4 - (-0.8225 + 0.0117))*0.1 << endl;
+} 
